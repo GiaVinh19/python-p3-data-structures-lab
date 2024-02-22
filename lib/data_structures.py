d4f264a9f972a9d0e13e08d236dd93a791f7fcd7
GiaVinhNguyen <vinh.g.nguyen19@gmail.com>
2024-02-22 03:09:23 -0500

    lab's status: spicy

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,69 @@ spicy_foods = [
     },
 ]
 
+print("")
+
 def get_names(spicy_foods):
-    pass
+    name_list = []
+    for spicy_food in spicy_foods:
+        name_list.append(spicy_food["name"])
+    print(name_list)
+    return name_list
+get_names(spicy_foods)
+
+print("")
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = []
+    for spicy_food in spicy_foods:
+        if spicy_food["heat_level"] > 5:
+            spiciest_foods.append(spicy_food)
+    print(spiciest_foods)
+    return spiciest_foods
+get_spiciest_foods(spicy_foods)
+
+print("")
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for spicy_food in spicy_foods:
+        pepper = "🌶" * spicy_food["heat_level"]
+        print(f'{spicy_food["name"]} ({spicy_food["cuisine"]}) | Heat Level: {pepper}')
+print_spicy_foods(spicy_foods)
+
+print("")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for spicy_food in spicy_foods:
+        if spicy_food["cuisine"] == cuisine:
+            print(spicy_food)
+            return spicy_food
+get_spicy_food_by_cuisine(spicy_foods, "Thai")
+
+print("")
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for spicy_food in spicy_foods:
+        if spicy_food["heat_level"] > 5:
+            pepper = "🌶" * spicy_food["heat_level"]
+            print(f'{spicy_food["name"]} ({spicy_food["cuisine"]}) | Heat Level: {pepper}')
+print_spiciest_foods(spicy_foods)
+
+print("")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    average_heat_level = 0
+    for spicy_food in spicy_foods:
+        average_heat_level += spicy_food["heat_level"]
+    average_heat_level = average_heat_level / (len(spicy_foods))
+    return int(average_heat_level)
+get_average_heat_level(spicy_foods)
+
+print("")
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    print(spicy_foods)
+    return(spicy_foods)
+create_spicy_food(spicy_foods, {"name": "Wasabi",
+                                "cuisine": "Japanese",
+                                "heat_level": 7,})
